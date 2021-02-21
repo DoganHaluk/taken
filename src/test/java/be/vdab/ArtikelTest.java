c71ebdab912c2c7eee498df1fda65a31a68cabb4
DoganHaluk <ha_look@yahoo.com>
2021-02-21 14:43:05 +0100

    JUnit Tasks

---
+++ src/test/java/be/vdab/ArtikelTest.java
@@ -0,0 +1,16 @@
+package be.vdab;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArtikelTest {
+
+    @Test
+    void prijsExclusief30enBtwPercentage6Moet31komma8PrijsInclusiefZijn() {
+        assertThat(new Artikel(BigDecimal.valueOf(30), BigDecimal.valueOf(6)).getPrijsInclusiefBtw()).isEqualByComparingTo("31.8");
+    }
+}
