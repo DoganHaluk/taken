6d6f9c6fab42a85fa95b89b99e794ab4d5066ab5
DoganHaluk <ha_look@yahoo.com>
2021-02-27 12:00:41 +0100

    WinstService added

---
+++ src/main/java/be/vdab/OpbrengstRepository.java
@@ -0,0 +1,7 @@
+package be.vdab;
+
+import java.math.BigDecimal;
+
+public interface OpbrengstRepository {
+    BigDecimal findTotaleOpbrengst();
+}
---
+++ src/main/java/be/vdab/WinstService.java
@@ -0,0 +1,17 @@
+package be.vdab;
+
+import java.math.BigDecimal;
+
+public class WinstService {
+    private final OpbrengstRepository opbrengstRepository;
+    private final KostRepository kostRepository;
+
+    public WinstService(OpbrengstRepository opbrengstRepository, KostRepository kostRepository) {
+        this.opbrengstRepository = opbrengstRepository;
+        this.kostRepository = kostRepository;
+    }
+
+    public BigDecimal getWinst(){
+        return opbrengstRepository.findTotaleOpbrengst().subtract(kostRepository.findTotaleKost());
+    }
+}
