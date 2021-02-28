fdce939ff3443b9b2d7ba7909c43b85d8877622c
DoganHaluk <ha_look@yahoo.com>
2021-02-28 16:59:52 +0100

    WinstService added

--- src/test/java/be/vdab/winst/WinstServiceTest.java
+++ src/test/java/be/vdab/winst/WinstServiceTest.java
@@ -2,18 +2,36 @@ package be.vdab.winst;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class WinstServiceTest {
+    @Mock
+    private OpbrengstRepository opbrengstRepository;
+    @Mock
+    private KostRepository kostRepository;
     private WinstService winstService;
+
     @BeforeEach
-    void beforeEach(){
-        winstService=new WinstService(new OpbrengstRepositoryStub(),new KostRepositoryStub());
+    void beforeEach() {
+        winstService = new WinstService(opbrengstRepository, kostRepository);
     }
+
     @Test
-    void winstIsOpbrengstMinKost(){
+    void winstIsOpbrengstMinKost() {
+        when(opbrengstRepository.findTotaleOpbrengst()).thenReturn(BigDecimal.valueOf(200));
+        when(kostRepository.findTotaleKost()).thenReturn(BigDecimal.valueOf(169));
+
         assertThat(winstService.getWinst()).isEqualByComparingTo("31");
+        verify(opbrengstRepository).findTotaleOpbrengst();
+        verify(kostRepository).findTotaleKost();
     }
 }
