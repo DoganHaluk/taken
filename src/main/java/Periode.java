cfe01aea18d910ea0649d4bc8d0341b1a92a8cf2
DoganHaluk <ha_look@yahoo.com>
2021-02-28 23:31:11 +0100

    WinstService added

---
+++ src/main/java/Periode.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+public class Periode {
+    private final LocalDate van;
+    private final LocalDate tot;
+
+    public Periode(LocalDate van, LocalDate tot) {
+        if(tot.isBefore(van)){
+            throw new IllegalArgumentException();
+        }
+        this.van = van;
+        this.tot = tot;
+    }
+
+    public boolean komtNa(Periode periode){
+        return tot.isAfter(periode.van);
+    }
+
+    public boolean overlaptMet(Periode periode){
+        return bevat(periode.van)||bevat(periode.tot);
+    }
+    public boolean bevat(LocalDate datum){
+        return (datum.isEqual(van)||datum.isAfter(van))&&(datum.isEqual(tot)||datum.isBefore(tot));
+    }
+}
